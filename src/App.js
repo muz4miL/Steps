9661faae5025d778f25123eb076001e481bbff44
Muzamil Shiraz <bcs230302211@imsciences.edu.pk>
2025-07-14 17:07:38 +0500

    Used useState for the very first Time!

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,15 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    step > 1 ? setStep(step - 1) : alert("No Previous step available!");
+  }
+
+  function handleNext() {
+    step < 3 ? setStep(step + 1) : alert("No Next step available!");
+  }
 
   return (
     <div className="steps">
@@ -16,14 +26,23 @@ export default function App() {
       </div>
 
       <p className="message">
-        Step {step} : {messages[step - 1]}
+        {step >= 1 && step <= 3
+          ? `step ${step}: ${messages[step - 1]}`
+          : "Invalid Step!"}
+        ``
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
